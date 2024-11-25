7416f28c17b5119fad560531c20efde7f13816e7
nqv96 <vuong09062002@gmail.com>
2024-11-25 21:52:28 +0700

    add cmd

--- Documents/z_Psutil_Process/Code/main.py
+++ Documents/z_Psutil_Process/Code/main.py
@@ -7,9 +7,10 @@ class ProcessManager:
     
     def refresh_processes(self):
         #  refresh processes( update )
-        self.processes =  [proc.as_dict(attrs=['pid', 'name', 'cpu_percent', 'memory_info', 'status',"username", 'memory_percent'])
-            for proc in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_info', 'status', 'username', 'memory_percent'])
-        ]
+        self.processes =  [proc.as_dict(attrs=['pid', 'name', 'cpu_percent', 'memory_info', 'status',"username", 'memory_percent', 'cpu_affinity', 'memory_full_info', 'memory_maps', 'num_threads','cwd'])
+            for proc in psutil.process_iter()]
+                # (['pid', 'name', 'cpu_percent', 'memory_info', 'status', 'username', 'memory_percent'])
+
     def calculate_total_memory_percent(self):
         # total memory percent (RSS)
         if not self.processes:
@@ -29,15 +30,13 @@ class ProcessManager:
     
     def display_processes(self):
         print(" "*50,f"{self.calculage_total_cpu_percent()}"," "*5,f"{self.calculate_total_memory_percent():.2f} %")
-        print(f"{'PID':<10}{'Name':<20}{'Username':<20}{'CPU (%)':<10}{'Memory (RSS)':<15}{'Status':<10}")
-        print("-" * 80)
+        print(f"{'PID':<10}{'Name':<20}{'Username':<20}{'CPU (%)':<10}{'Memory (RSS)':<15}{'Status':<10} {'CWD'}")
+        print("-" * 100)
         for proc in self.processes:
             print(f"{proc['pid']:<10}{proc['name']:<20}{proc['username']:<20}{proc['cpu_percent']:<10.2f}"
-                  f"{proc['memory_info'].rss / (1024 * 1024):<15.2f}{proc['status']:<10}")
+                  f"{proc['memory_info'].rss / (1024 * 1024):<15.2f}{proc['status']:<10} {proc['cwd']}")
 
 if __name__ == "__main__":
     pm = ProcessManager()
     pm.display_processes()
-        
-    
     
